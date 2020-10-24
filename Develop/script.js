9d9b2ca42429e6811544a4ad232819e8c3bbbb19
Cody Chen <codychen1@gmail.com>
2020-10-24 17:26:36 -0600

    added time and date from moments

--- Develop/script.js
+++ Develop/script.js
@@ -5,33 +5,31 @@ var timeSlots = document.getElementById("containerId");
 var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 //FETCH API
-function getApi() {
-  // Insert the API url to get a list of your repos
-  var requestUrl = "https://api.github.com/users/codyops/repos";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var timeAndDate = document.createElement("h3");
-      timeAndDate.textContent = data[0].html_url;
-      currentDate.appendChild(timeAndDate);
-    });
-}
-getApi();
+var date = document.createElement("h3");
+var time = document.createElement("p");
+date.textContent = moment().format("dddd, MMMM Do YYYY");
+time.textContent = moment().hours();
+
+currentDate.append(date, time);
 
 //CREATING TIMESLOTS
 function createTimeSlots() {
   for (var i = 0; i < time.length; i++) {
     var timeList = document.createElement("div");
+
     timeList.textContent = time[i];
     timeSlots.appendChild(timeList);
   }
 }
 
-createTimeSlots();
-//LOCAL STORAGE
-// var userEvent = timeSlots.value.trim();
+//EVENTS
+$(".btn").on("click", function () {
+  console.log($(this).siblings(".text").val());
+  console.log($(this).parent().attr("id"));
+});
+//key and value to local storage
 
-// localStorage.setItem("userEvent", JSON.stringify(userEvent));
+//ENTRY POINT
+// createTimeSlots();
+// getApi();
