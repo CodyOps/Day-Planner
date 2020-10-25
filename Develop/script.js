c927459f500d36302763eda321940cb0c5477fed
Cody Chen <codychen1@gmail.com>
2020-10-25 16:26:10 -0600

    added ready function to document

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,12 @@ var currentDate = document.getElementById("currentDay");
 var timeSlots = document.getElementById("containerId");
 var saveButton = document.getElementsByClassName(".saveBtn");
 
+//Array for all time slots during regular business hours
 var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 //FETCH API
 
+//Creates the current time and date pulled from moment.js
 var date = document.createElement("h3");
 var time = document.createElement("p");
 date.textContent = moment().format("dddd, MMMM Do YYYY");
@@ -25,24 +27,28 @@ function createTimeSlots() {
 }
 
 //EVENTS
-$(".saveBtn").on("click", function () {
-  console.log($(this).siblings(".text").val());
-  console.log($(this).parent().attr("id"));
+$(document).ready(function () {
+  $(".text").text = localStorage.getItem(eventTime, eventText);
+  //after the document is loaded, check local storage for any saved key value pairs and load it into the textArea for the user
+  $(".saveBtn").on("click", function () {
+    //run the function after the user clicks a save button
+    console.log($(this).siblings(".text").val());
+    console.log($(this).parent().attr("id"));
 
-  var eventText = $(this).siblings(".text").val();
-  var eventTime = $(this).parent().attr("id");
+    var eventText = $(this).siblings(".text").val();
+    var eventTime = $(this).parent().attr("id");
 
-  if (eventText === "") {
-    alert("Event text cannot be blank");
-  } else {
-    alert("Event Saved");
-  }
-  console.log(eventTime, eventText);
-  localStorage.setItem(eventTime, eventText);
+    if (eventText === "") {
+      alert("Event text cannot be blank");
+    } else {
+      alert("Event Saved");
+    }
+    console.log(eventTime, eventText);
+    localStorage.setItem(eventTime, eventText);
 
-  $(this).siblings(".text").text = localStorage.getItem(eventText);
+    $(this).siblings(".text").text = localStorage.getItem(eventText);
+  });
 });
-
 //ENTRY POINT
 // createTimeSlots();
 // getApi();
