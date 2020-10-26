b8efb62ef4ce37d01187ee568ac22f52bcaab3a3
Cody Chen <codychen1@gmail.com>
2020-10-25 19:43:49 -0600

    added JSON

--- Develop/script.js
+++ Develop/script.js
@@ -18,28 +18,6 @@ currentDate.append(date, time);
 
 $(document).ready(function () {
   //creates a variable to go to local storage and grab the key and value
-  // var myCoolObject = {
-  //   events: [
-  //     {
-  //       event: "some event",
-  //     },
-  //     {
-  //       event: "some other event",
-  //     },
-  //   ],
-  // };
-  // localStorage.setItem("allEvents", JSON.stringify(myCoolObject));
-  // var retrievedObject = localStorage.getItem("allEvents");
-  // console.log(retrievedObject);
-  // console.log(JSON.parse(retrievedObject));
-  function saveRow() {
-    for (var i = 0; i < time.length; i++) {
-      if (localStorage.getItem(time[i])) {
-        var retrievedRow = localStorage.getItem("eText");
-        text.value = JSON.parse("eText");
-      }
-    }
-  }
 
   if (time < date.value) {
     textArea.setAttribute = ".past";
@@ -49,8 +27,8 @@ $(document).ready(function () {
 
   $(".saveBtn").on("click", function () {
     //run the function after the user clicks a save button
-    console.log($(this).siblings(".text").val());
-    console.log($(this).parent().attr("id"));
+    // console.log($(this).siblings(".text").val());
+    // console.log($(this).parent().attr("id"));
 
     var eventText = $(this).siblings(".text").val();
     var eventTime = $(this).parent().attr("id");
@@ -61,11 +39,21 @@ $(document).ready(function () {
       alert("Event Saved");
     }
     console.log(eventTime, eventText);
-    localStorage.setItem("eText", JSON.stringify(eventText));
-    localStorage.setItem("eTime", JSON.stringify(eventTime));
+    localStorage.setItem("time", JSON.stringify(eventText));
+    // localStorage.setItem("eTime", JSON.stringify(eventTime));
+
+    function saveRow() {
+      for (var i = 0; i < time.length; i++) {
+        if (localStorage.getItem(time[i]) !== null) {
+          localStorage.getItem("time");
+          JSON.parse("eventText");
+        }
+      }
+    }
+    saveRow();
   });
 
-  saveRow();
+  localStorage.getItem("time", JSON.parse(eventText));
 });
 //ENTRY POINT
 
